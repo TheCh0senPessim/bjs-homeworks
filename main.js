5fc29947feccd615edbd60a2d2ad64d0ac291687
Nikita <klosr@list.ru>
2019-05-24 11:58:07 +0300

    2nd task

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ let physics =3;
 let average = (algebra + geography + physics) / 3;
 
 console.log("Среднее значение = " + average);
+
+// Второе задание:
+
+let name = "Никита";
+let message = `Привет, мир! Меня зовут ${name} .`;
+console.log(message);
