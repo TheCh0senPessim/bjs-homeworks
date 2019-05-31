bc99d5bce465026ece58414c308e912eff17991a
Nikita <klosr@list.ru>
2019-05-31 21:11:03 +0300

    Исправил ошибку в первом задание + черновой вариант второго задания

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -5,14 +5,15 @@ a, b,c - коэффициенты квадратного уравнения 2x^2
 
 function getResult( a, b, c) {
   let x;
+  let D = b**2 - 4*a*c;
   if (b**2 - 4*a*c < 0) {
-    console.log("Нет корней");
-  } else if (b**2 - 4*a*c === 0) {
+    console.log(`Нет корней`);
+  } else if (D === 0) {
     x = - b / (2*a);
-    console.log("Дискриминант равен нулю, значение: " + x);
-  } else if (b**2 - 4*a*c > 0) {
-    x = [(-b + Math.sqrt((b**2 - 4*a*c)))/(2*a) ,(- b - Math.sqrt((b**2 - 4*a*c)))/(2*a) ];
-    console.log("Дискриминант больше нуля, значение: " + x);
+    console.log(`Дискриминант равен нулю, значение: ${x}`);
+  } else if (D > 0) {
+    x = [(-b + Math.sqrt(D))/(2*a) ,(- b - Math.sqrt(D))/(2*a) ];
+    console.log(`Дискриминант больше нуля, значение: ${x}`);
   }
   return x;  
 }
@@ -20,3 +21,17 @@ function getResult( a, b, c) {
 getResult(2,4,-3);
 // getResult(1,2,1);
 // getResult(1,1,1);
+
+//Второе задание:
+
+function getAverageMark(...marks) {
+  let total = 0;
+  for (let i = 4; i >=0; i--) {
+  total = total + marks[ i ];
+}
+total = total/5;
+return total;
+}
+
+
+getAverageMark(4,4,5,4,4)
